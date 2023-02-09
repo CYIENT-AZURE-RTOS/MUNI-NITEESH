587d114a8fc735e3f515636608421c2bd6462b13
MuniNiteesh <108974778+MuniNiteesh@users.noreply.github.com>
2023-02-09 15:28:05 +0530

    Create queue.c

---
+++ DAY6/queue.c
@@ -0,0 +1,189 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    app_threadx.c
+  * @author  MCD Application Team
+  * @brief   ThreadX applicative file
+  ******************************************************************************
+    * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_threadx.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "main.h"
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+TX_THREAD ThreadOne;
+TX_THREAD ThreadTwo;
+
+TX_QUEUE queue_pointer;
+extern UART_HandleTypeDef huart1;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+void ThreadOne_Entry(ULONG thread_input);
+void ThreadTwo_Entry(ULONG thread_input);
+void Mainthread_Entry(ULONG thread_input);
+void App_Delay(uint32_t Delay);
+/* USER CODE END PFP */
+
+/**
+  * @brief  Application ThreadX Initialization.
+  * @param memory_ptr: memory pointer
+  * @retval int
+  */
+UINT App_ThreadX_Init(VOID *memory_ptr)
+{
+  UINT ret = TX_SUCCESS;
+  TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
+
+   /* USER CODE BEGIN App_ThreadX_MEM_POOL */
+  (void)byte_pool;
+  /* USER CODE END App_ThreadX_MEM_POOL */
+
+  /* USER CODE BEGIN App_ThreadX_Init */
+  CHAR *pointer,*pointer2,*pointer3;
+
+    /* Allocate the stack for ThreadOne.  */
+    if (tx_byte_allocate(byte_pool, (VOID **) &pointer,
+                         APP_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+    {
+      ret = TX_POOL_ERROR;
+    }
+
+    /* Create ThreadOne.  */
+    if (tx_thread_create(&ThreadOne, "Thread One", ThreadOne_Entry, 0,
+                         pointer, APP_STACK_SIZE,
+                         THREAD_ONE_PRIO, THREAD_ONE_PREEMPTION_THRESHOLD,
+                         1, TX_AUTO_START) != TX_SUCCESS)
+    {
+      ret = TX_THREAD_ERROR;
+    }
+
+    /* Allocate the stack for ThreadTwo.  */
+    if (tx_byte_allocate(byte_pool, (VOID **) &pointer2,
+                         APP_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+    {
+      ret = TX_POOL_ERROR;
+    }
+    /* Create ThreadTwo.  */
+    if (tx_thread_create(&ThreadTwo, "Thread Two", ThreadTwo_Entry, 0,
+                         pointer2, APP_STACK_SIZE,
+                         THREAD_TWO_PRIO, THREAD_TWO_PREEMPTION_THRESHOLD,
+                         1, TX_AUTO_START) != TX_SUCCESS)
+    {
+      ret = TX_THREAD_ERROR;
+    }
+    /* Allocate the stack for ThreadQueue.  */
+    if (tx_byte_allocate(byte_pool, (VOID **) &pointer3,
+    		QUEUE_DATA_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+    {
+      ret = TX_POOL_ERROR;
+    }
+    if(TX_SUCCESS!=(tx_queue_create(&queue_pointer,"queue pointer",16,(void *)pointer3,QUEUE_DATA_SIZE)))
+    {
+    	ret=TX_QUEUE_ERROR;
+    }
+  /* USER CODE END App_ThreadX_Init */
+
+  return ret;
+}
+
+  /**
+  * @brief  MX_ThreadX_Init
+  * @param  None
+  * @retval None
+  */
+void MX_ThreadX_Init(void)
+{
+  /* USER CODE BEGIN  Before_Kernel_Start */
+
+  /* USER CODE END  Before_Kernel_Start */
+
+  tx_kernel_enter();
+
+  /* USER CODE BEGIN  Kernel_Start_Error */
+
+  /* USER CODE END  Kernel_Start_Error */
+}
+
+/* USER CODE BEGIN 1 */
+/**
+  * @brief  Function implementing the ThreadOne thread.
+  * @param  thread_input: Not used
+  * @retval None
+  */
+void ThreadOne_Entry(ULONG thread_input)
+{
+
+  char c[]="\n\rQUEUE thread\n\r";
+  UINT status;
+  while(1)									/* Infinite loop */
+  {
+	  HAL_UART_Transmit(&huart1, "\n\r1st msg\n\r", 15, 100);
+	  status=tx_queue_send(&queue_pointer,c,TX_WAIT_FOREVER);
+	  if(status==TX_SUCCESS)
+	  {
+	  HAL_UART_Transmit(&huart1,"\n\r 1st msg sent \n\r",22,100);
+	  tx_thread_sleep(100);
+	  }
+  }
+}
+void ThreadTwo_Entry(ULONG thread_input)
+{
+	char rx[]="\n\r  \n\r";
+	  UINT status;
+	  while(1)									/* Infinite loop */
+	  {
+		  HAL_UART_Transmit(&huart1, "\n\r2nd msg\n\r", 15, 100);
+		  status=tx_queue_receive(&queue_pointer,rx,TX_WAIT_FOREVER);
+		  if(status==TX_SUCCESS)
+		  {
+		  HAL_UART_Transmit(&huart1,rx,50,100);
+		  tx_thread_sleep(100);
+		  }
+	  }
+}
+
+/**
+  * @brief  Application Delay function.
+  * @param  Delay : number of ticks to wait
+  * @retval None
+  */
+void App_Delay(uint32_t Delay)
+{
+  UINT initial_time = tx_time_get();
+  while ((tx_time_get() - initial_time) < Delay);
+}
+/* USER CODE END 1 */
